3877e24f465a33d35a731fe63b5b7266bb7ef8db
KimFrost <example@example.com>
2024-06-04 10:29:57 -0300

    exercise 1.4 making things a little more dinamic

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -16,35 +16,41 @@ const Content = ({ data }) => {
   </div>
 }
 
-const Total = ({ numberOfExercises }) => {
-  return <p>Number of exercises {numberOfExercises}</p>
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+const Total = ({ parts }) => {
+  let  inititalValue = 0
+  return <p>
+    Number of exercises { parts.reduce( (n, m) => {
+      return n + m.exercises
+      }, inititalValue)}
+  </p>
 }
 
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const arrayOfParts = [part1, part2, part3]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header courseName={course} />
 
-      <Content data={ arrayOfParts } />
+      <Content data={ parts } />
 
-      <Total numberOfExercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Total parts={ parts } />
     </div>
   )
 }
