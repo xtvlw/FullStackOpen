bd6b6ad0d6c82690adfab221939d4516b6c473e7
KimFrost <example@example.com>
2024-06-10 13:25:38 -0300

    adding exercise 2.2

--- part-2/exercise-2/src/components/DisplayCourseContent.jsx
+++ part-2/exercise-2/src/components/DisplayCourseContent.jsx
@@ -1,11 +1,20 @@
 
 
+const totalNumberOfExercise = (data) => {
+    return data.reduce((m, n) => {
+        return m + n.exercises
+    }, 0)
+}
+
 const DisplayCourseContent = ({ data }) => {
+    const numberOfExercises = totalNumberOfExercise(data)
+
     return(
         <div>
             {data.map(item => {
                 return <p key={item.id}>{item.name} {item.exercises}</p>
             })}
+            <p>total of {numberOfExercises} exercises</p>
         </div>
     )
 }
