266d7419c009e078980924379cbfc03bc8f98d83
KimFrost <example@example.com>
2024-06-13 09:42:16 -0300

    adding exercise 2.11, adding the basic data fetch form server

--- part-2/exercises-2.6_to_2.11/src/App.jsx
+++ part-2/exercises-2.6_to_2.11/src/App.jsx
@@ -1,22 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 import Form from "./components/Form.jsx"
 import FormInput from "./components/FormInput.jsx"
 import DisplayContacts from "./components/DisplayContacts.jsx"
 import Filter from "./components/Filter.jsx"
 
+
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filteredContact, setFilteredContact] = useState(persons)
+  const [filteredContact, setFilteredContact] = useState([])
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(res => {
+      setPersons(res.data)
+      setFilteredContact(res.data)
+    })
+  }, [])
+
 
   const isPersonAdded = name => {
     const personCheck = persons.find(p => p.name === name)
